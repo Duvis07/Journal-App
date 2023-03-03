dab0b94a41767e38f809f3b388226d27ada7ed64
duvan.botero <duvis07>
2023-03-03 12:17:14 -0500

    Se termina login y registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { Google } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import React from "react";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Registro">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre Completo"
+              type="text"
+              placeholder="Nombre Completo"
+              fullWidth
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="Email@gmail.com"
+              fullWidth
+            ></TextField>
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="********"
+              fullWidth
+            ></TextField>
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta? </Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};

d1da16704de0369e43d6f62cd107aa4e002f7c89
duvan.botero <duvis07>
2023-03-04 09:40:08 -0500

    Se termina app con material Mui

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,22 @@
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { Navbar, SideBar } from "../components";
+
+const drawerWidth = 280;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Navbar drawerWidth={drawerWidth} />
+
+      <SideBar drawerWidth={drawerWidth} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
+        <Toolbar />
+
+        {children}
+      </Box>
+    </Box>
+  );
+};
